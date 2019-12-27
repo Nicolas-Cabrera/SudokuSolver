fe16032008cac46fcb9ba7fa0e7d8fad970a1199
Nicolas Cabrera <nicolascc1@hotmail.com>
2019-12-27 13:37:47 +0000

    push for szymon to have a look

--- src/Input/NumbersInput.js
+++ src/Input/NumbersInput.js
@@ -5,27 +5,37 @@ export default function NumbersInput() {
 
 	const [nums, setNums] = useState([]);
 
+	function handleChange(e, index) {
+		if(index === 0) {
+			let first = e.target.value;
+		} else if(index === 1) {
+			let second = e.target.value;
+		} else {
+			let third = e.target.value;
+		}
+		//let arr = [first, second, third];
+	}
+
 	return (
 		<div>
 			<h1>Enter numbers</h1>
-			<form className='width-size'>
-				<input type='number' onChange={(e) => setNums(e.target.value)}></input>
-				<input type='number'></input>
-				<input type='number'></input>
-			</form>
-			<form className='width-size'>
-				<input type='number'></input>
-				<input type='number'></input>
-				<input type='number'></input>
-			</form>
-			<form className='width-size'>
-				<input type='number'></input>
-				<input type='number'></input>
-				<input type='number'></input>
-			</form>
-
+				<form className='width-size'>
+					<input type='text' maxLength='1' onChange={(e) => handleChange(e, 0)}></input>
+					<input type='text' maxLength='1' onChange={(e) => handleChange(e, 1)}></input>
+					<input type='text' maxLength='1' onChange={(e) => handleChange(e, 2)}></input>
+				</form>
+				<form className='width-size'>
+					<input type='text' maxLength='1'></input>
+					<input type='text' maxLength='1'></input>
+					<input type='text' maxLength='1'></input>
+				</form>
+				<form className='width-size'>
+					<input type='text' maxLength='1'></input>
+					<input type='text' maxLength='1'></input>
+					<input type='text' maxLength='1'></input>
+				</form>
+			<h2></h2>
 			<div>
-				<h3>{nums}</h3>
 			</div>
 
 		</div>
